6e71cccb987850af6b36c81d5e921b104c3f3ba2
Tomii1024 <tomcsi009@gmail.com>
2022-03-23 10:15:46 +0100

    1000-rel való oszthatóság ellenőrzése

--- hahuapi/routes/hahu.js
+++ hahuapi/routes/hahu.js
@@ -16,13 +16,23 @@ router.post('/', function(req, res, next) {
   const arFt= req.body.arFt;
   const kepUrl= req.body.kepUrl;
 
-  const hirdetes = new Hirdetes({_id, kategoria, cim, leiras, hirdetesDatuma, serulesmentes, arFt, kepUrl});
+  try {
+    if (arFt % 1000 != 0) {
+      throw Error("Az ár nem osztható 1000-el!")
+    }
+    const hirdetes = new Hirdetes({_id, kategoria, cim, leiras, hirdetesDatuma, serulesmentes, arFt, kepUrl});
   hirdetes
   .save()
   .then(res.json({
       "message":"A rekord rögzítése sikeres!"
   }))
   .catch(err => console.log(err))
+  } catch (error) {
+    res.status(400).json({
+      'error': error.message,
+    })
+  }
+  
 });
 
 module.exports = router;
