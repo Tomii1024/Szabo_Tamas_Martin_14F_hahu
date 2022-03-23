6b90d4efee1e3fff88f8e645758e63809e1c000b
Tomii1024 <tomcsi009@gmail.com>
2022-03-23 09:03:56 +0100

    Az alapértelmezett értékek beállítása a sémában

--- hahuapi/models/hirdetes.js
+++ hahuapi/models/hirdetes.js
@@ -5,6 +5,10 @@ const Schema = mongoose.Schema;
 
 const hirdetesSchema = new Schema({
   _id: Number,
+  kategoria: {
+      type: Number, 
+      default: 1
+  },
   kategoria: Number,
   cim: {
     type: String,
@@ -18,6 +22,7 @@ const hirdetesSchema = new Schema({
   },
   hirdetesDatuma: {
     type: Date,
+    default: Date.now()
   },
   serulesmentes: Boolean,
   arFt: {
